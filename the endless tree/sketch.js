f245c3e35cdbab0c050f2873660e6dbb4f654f9a
shinreeYu <xy2880@nyu.edu>
2024-10-23 18:35:41 +0800

    project finished

--- the endless tree/sketch.js
+++ the endless tree/sketch.js
@@ -14,12 +14,17 @@ let leafSize = [10, 10, 10, 10, 10];
 let leafsize = [5, 5, 5, 5, 5];
 let planetTp = [225, 225, 225, 225, 225];
 let leafGrow = [true,true,true,true,true]
-let leafCenterX=[0,0,0,0,0]
+let leafCenterX = [0,0,0,0,0]
+let leafCenterY = [0,0,0,0,0]
+let leafControlUp = [0,0,0,0,0]
+let leafControlDown = [0,0,0,0,0]
+let leafControlRight = [0,0,0,0,0]
+let leafControlLeft = [0,0,0,0,0]
+let transprc = [200,200]
 
 function setup() {
-    let cnv=createCanvas(800,500)
-    cnv.parent("p5-canvas-container")
   loopNum = random(3, 5);
+  createCanvas(800, 500);
   for (i = 1; i <= loopNum; i++) {
     planetColorR[i - 1] = random(175, 225);
     planetColorG[i - 1] = random(100, 200);
@@ -30,10 +35,11 @@ function setup() {
 }
 
 function draw() {
+  
   noStroke();
+  // createCanvas(800, 500);
   noFill();
   stroke(100, 50, 180);
-  
   push();
   translate(width / 2, height / 2);
   //create the track of stars
@@ -42,7 +48,12 @@ function draw() {
   }
   background(50, 25, 90, 235);
   noStroke();
-  fill(255, 255, 100, 20);
+  fill(255, 255, 100,transprc[0]);
+  text('Dear scientist, good luck with your laser and observe carefully!',-175,-150)
+  // for(i=0;i<=20000000;i++){
+  //   transprc[0]-=0.0000005
+  // }
+  fill(255,255,100,20)
   //create the sun&sunshine
   for (i = 1; i <= 15; i++) {
     circle(0, 0, 10 + 3 * i);
@@ -91,12 +102,16 @@ function draw() {
       } else {
          noStroke()
               fill(100,150,100,planetTp[mouseNum[i]-1])
-              ellipse(((50 + (430 / round(loopNum)) * mouseNum[i] + planetDia[mouseNum[i] - 1] + 150-25*i) *
-                sin(radiumRd[mouseNum[i] - 1])) /
-                2,
-              ((50 + (430 / round(loopNum)) * mouseNum[i] + planetDia[mouseNum[i] - 1] + 150-25*i) *
-                cos(radiumRd[mouseNum[i] - 1])) /
-                2,leafSize[i],leafsize[i])
+        leafCenterX[i]=((50 + (430 / round(loopNum)) * mouseNum[i] + planetDia[mouseNum[i] - 1] + 150-25*i) *
+                sin(radiumRd[mouseNum[i] - 1])) /2
+        leafCenterY[i]=((50 + (430 / round(loopNum)) * mouseNum[i] + planetDia[mouseNum[i] - 1] + 150-25*i) *
+                cos(radiumRd[mouseNum[i] - 1])) /2
+              ellipse(leafCenterX[i],leafCenterY[i],
+                      leafSize[i],leafsize[i])
+        ellipse(leafCenterX[i],leafCenterY[i]-5*leafControlUp[i],10+5*leafControlUp[i],10+10*leafControlUp[i])
+    ellipse(leafCenterX[i],leafCenterY[i]-5*leafControlDown[i],10+5*leafControlDown[i],10+10*leafControlDown[i])
+        ellipse(leafCenterX[i]+5*leafControlRight[i],leafCenterY[i],10-10*leafControlRight[i],10-5*leafControlRight[i])
+        ellipse(leafCenterX[i]+5*leafControlLeft[i],leafCenterY[i],10-10*leafControlLeft[i],10-5*leafControlLeft[i])
                 if(leafSize[i]<=100){
           if(mouseX >=
             ((50 +(430 / round(loopNum)) * mouseNum[i] +
@@ -115,11 +130,30 @@ function draw() {
               planetDia[mouseNum[i] - 1] +150 -25 * i) *
               cos(radiumRd[mouseNum[i] - 1])) /2 +50 +height / 2 &&
           mouseIsPressed == true
-                  ){leafGrow[i]= !leafGrow[i]}
+                  ){leafGrow[i]= false}
           else if(leafGrow[i] == true){leafSize[i]+=0.1
                     leafsize[i]+=0.06}}
-            if(leafSize[i]>100){planetTp[mouseNum[i]-1]-=10
-                planetDia[mouseNum[i]-1]+=5}}
+        fill(255,255,100,transprc[1])
+          if(leafGrow[i]==false){
+            transprc[0]=0
+       text('Great! What about trying the keys?',-150,-100)
+  
+  
+            if(key=='ArrowUp' && keyIsPressed == true){
+              leafControlUp[i]+=1
+          }
+          if(keyIsPressed == true && key=='ArrowDown'){
+            leafControlDown[i]-=1
+          }
+          if(keyIsPressed == true && key=='ArrowRight'){
+            leafControlRight[i]+=1
+          }
+          if(keyIsPressed == true && key=='ArrowLeft'){
+            leafControlLeft[i]-=1
+          }}
+            if(leafControlUp[i]>=10||leafControlDown[i]<=-10||leafControlRight[i]>=10||leafControlLeft[i]<=-10){planetTp[mouseNum[i]-1]-=10
+                planetDia[mouseNum[i]-1]+=5
+                                                                                                               transprc[1]=transprc[1]-200/loopNum}}
             noStroke();
       //after tree finish growing, mouse changes when close to leaf
         if (
@@ -161,8 +195,6 @@ function draw() {
   stroke(200, 50, 50);
   strokeWeight(8);
   point(mouseX, mouseY);
-  // line(mouseX - 5, mouseY, mouseX + 5, mouseY);
-  // line(mouseX, mouseY - 5, mouseX, mouseY + 5);
   if (
     mouseIsPressed == true &&
     mouseX >= Xmin[0] &&
